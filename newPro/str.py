be48a96f9c5ded7f1e803f8dba71912f4125b5ca
ichigolin <15800758562@163.com>
2018-06-08 15:08:32 +0800

    wsp bendan
    str

---
+++ newPro/str.py
@@ -0,0 +1,41 @@
+s = input()
+s.find('f')
+s.find('r')
+s.find('i')
+s.find('e')
+s.find('n')
+s.find('d')
+if s.find('f')>=0 and s.find('r')>=0 and s.find('i')>=0 and s.find('e')>=0 and s.find('n')>=0 and s.find('d')>=0:
+    print('friend')
+else:
+    print("there is no found 'friend' in your input")
+s.count('f')
+s.count('r')
+s.count('i')
+s.count('e')
+s.count('n')
+s.count('d')
+min=s.count('f')
+if min>s.count('f'):
+    min=s.count('f')
+elif min>s.count('r'):
+    min=s.count('r')
+elif min>s.count('i'):
+    min=s.count('i')
+elif min>s.count('e'):
+    min=s.count('e')
+elif min>s.count('n'):
+    min=s.count('n')
+elif min>s.count('d'):
+    min=s.count('d')
+print(min)
+
+
+
+
+
+
+
+
+
+
